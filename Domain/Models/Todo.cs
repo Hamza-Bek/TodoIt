943fcf09f7182a0db75f7e3cc20db18811a7c734
Rasheed K Mozaffar <rasheedkmozaffar@hotmail.com>
2025-02-10 01:17:41 +0200

    Correc the data models

--- Domain/Models/Todo.cs
+++ Domain/Models/Todo.cs
@@ -1,23 +1,16 @@
 ﻿using Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Models
 {
     public class Todo : EntityBase
     {
         public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;               
+        public string Description { get; set; } = string.Empty;
         public Priority Priority { get; set; }
-        public bool IsCompleted { get; set; }
-        public bool IsPined { get; set; }
-        public bool IsOverdue { get; set; }
-        public Guid OwnerId { get; set; }        
+        public bool Completed { get; set; }
+        public bool Pinned { get; set; }
+        public bool Overdue { get; set; }
+        public Guid OwnerId { get; set; }
         public ApplicationUser? Owner { get; set; }
-
     }
 }
