d0f6758fc418fd83615f8f58f031e72f2b5d7a66
Hamza Bek <hamzamrbek@gmail.com>
2025-07-12 13:28:27 +0300

    Add theme provider logic

---
+++ WebUI/Themes/Theme.cs
@@ -0,0 +1,10 @@
+﻿namespace WebUI.Themes
+{
+    public class Theme
+    {
+        public string PrimaryColor { get; set; } = "#007bff";
+        public string SecondaryColor { get; set; } = "#6c757d";
+        public string SuccessColor { get; set; } = "#28a745";
+        public string DangerColor { get; set; } = "#dc3545";
+    }
+}
